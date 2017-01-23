d987baf539714495810fd1d24ab18e4536d5e7f7
Victor Dmitriyev <mrvvitek@gmail.com>
2017-01-24 02:40:33 +0300

    A lot of cleaning up

--- reformat.c
+++ reformat.c
@@ -44,9 +44,37 @@ This is just overkill for 4 bytes:
 #define string4ToInt32(a,b,c,d) string4ToInt32LE(a,b,c,d)
 
 /* NOTE: relies on that pointers when cast from (uint8_t *) to (uint32_t *)
+and (uint16_t *)
 work as usual when dereferenced, which, as far as I can tell,
 is Undefined Behavior, and can be compiled in unexpected ways
 so, you should probably compile with -fno-strict-aliasing */
+
+static size_t findChunk(uint32_t chunkMark, FILE *file, char *fileName)
+{
+    size_t chunkLength = 0;
+    for(size_t ioStatus = 1; ioStatus > 0;)
+    {
+        uint32_t chunkHead[2];
+        ioStatus = fread(chunkHead, 1, 4*2, file);
+        if(ioStatus < 4*2)
+        {
+            printf("%s: Can't find \"%.4s\" chunk.\n",
+                    fileName, (char *)&chunkMark);
+            break;
+        }
+        if(chunkHead[0] == chunkMark)
+        {
+            chunkLength = chunkHead[1];
+            return chunkLength;
+        }
+        if(fseek(file, chunkHead[1], SEEK_CUR))
+        {
+            printf("%s: Can't read enough of file.\n", fileName);
+            break;
+        }
+    }
+    return chunkLength;
+}
 
 /* TODO: if input is directory, process all the files inside */
 /* TODO: if stored length is less then current file length, make file smaller */
@@ -67,7 +95,7 @@ int main(int argc, char **argv)
                 , argv[0]);
         return -1;
     }
-    static uint16_t waveId = 0x11;
+    static uint16_t waveId = 0x0011;
     for(size_t i = 1; i < (size_t)argc; ++i)
     {
         FILE *file = fopen(argv[i], "r+b");
@@ -118,45 +146,22 @@ int main(int argc, char **argv)
             }
         }
 
-        size_t fmtLength = 0;
-        /* find "fmt " chunk */
-        while(ioStatus > 0)
-        {
-            uint32_t chunkHead[2];
-            ioStatus = fread(chunkHead, 1, 4*2, file);
-            if(ioStatus < 4*2)
-            {
-                printf("%s: Can't find fmt chunk.\n", argv[i]);
-                break;
-            }
-
-            if(chunkHead[0] == fmtMark)
-            {
-                fmtLength = chunkHead[1];
-                break;
-            }
-            if(fseek(file, chunkHead[1], SEEK_CUR))
-            {
-                printf("%s: Can't read enough of file.\n", argv[i]);
-                break;
-            }
-        }
+        size_t fmtLength = findChunk(fmtMark, file, argv[i]);
         if(fmtLength == 0)
         {
-            fclose(file);
-            continue;
+            fclose(file); continue;
         }
-        if(fmtLength < 20)
+        if(fmtLength != 24)
         {
-            printf("%s: File has wrong format of data.\n", argv[i]);
-            fclose(file);
-            continue;
+            printf("%s: File has wrong format of data. "
+                    "fmt chunk is of unexpected size (%u bytes).\n",
+                    argv[i], fmtLength);
+            fclose(file); continue;
         }
-        size_t fmtOffset = ftell(file);
 
         /* IMA ADPCM fmt chunk as described in specification
            fmtLength = 20;
-           0  w uint16_t wFormatTag = 0x0011;
+           0 rw uint16_t wFormatTag = 0x0011;
            2  r uint16_t nChannels;
            4    uint32_t nSamplesPerSec;
            8    uint32_t nAvgBytesPerSec = (rounded up)
@@ -186,195 +191,113 @@ int main(int argc, char **argv)
 
         /* IMA ADPCM fmt chunk adapted to multichannel wave specification
            TODO: add short description */
-        uint8_t fmtDescription[18];
-        ioStatus = fread(fmtDescription, 18, 1, file);
-        if(ioStatus == 0)
-        {
-            printf("%s: Can't read enough of file.\n", argv[i]);
-            fclose(file);
-            continue;
-        }
-        uint16_t formatID = *(uint16_t *)fmtDescription;
-        uint16_t channels = *(uint16_t *)(fmtDescription+2);
-        uint16_t align = *(uint16_t *)(fmtDescription+12);
-        uint16_t sampleBits = *(uint16_t *)(fmtDescription+14);
-        if(!(( formatID == 0x02 || formatID == 0x11) && sampleBits == 4))
-        {
-            printf("%s: File doesn't seem to be (Wwise) IMA ADPCM wave."
-                    " It has format ID 0x%02x and bit depth %u.\n",
-                    argv[i], formatID, sampleBits);
-            fclose(file);
-            continue;
-        }
 
-        /* TODO: dwChannelMask */
-        /* NOTE: not all Wwise WAVE files are right,
-         * some of them are marked bigger size then they are.
-         * NOTE: Content of extra section in fmt chunk (extended header)
-         * cbSize is 6
-         * 0x000004000000:
-         * file contains normal IMA ADPCM (don't change data chunk!)
-         *
-         * 0x000003000000
-         * 0x18000b000000
-         * 0x18000c000000
-         * 0x10000b000000
-         * 0x10000c000000
-         * "clustered" IMA ADPCM
-         * it is IMA ADPCM, but samples
-         * (not samples but uint32 words worth of data) are not interleaved,
-         * they are clustered one channel after another */
-        uint16_t extraSize = *(uint16_t *)(fmtDescription+16);
-        static uint8_t extraSectionData[6];
-        uint8_t *extraSection;
-        if(extraSize <= 6)
-            extraSection = extraSectionData;
-        else
-            extraSection = malloc(extraSize);
-        ioStatus = fread(extraSection, extraSize, 1, file);
-        if(ioStatus == 0)
-        {
-            printf("%s: Can't read enough of file.\n", argv[i]);
-            if(extraSize > 6) free(extraSection);
-            fclose(file);
-            continue;
-        }
-#if 0
-        printf("%s ch:%u ", argv[i], channels);
-        for(size_t n = 0; n < extraSize; ++n)
-            printf("%02x", extraSection[n]);
-        printf("\n");
-#endif
-
-        if(fseek(file, fmtOffset, SEEK_SET))
+        long fmtOffset = ftell(file);
+        if(fmtOffset < 0)
         {
             printf("%s: Error using file?!\n", argv[i]);
-            if(extraSize > 6) free(extraSection);
-            fclose(file);
-            continue;
+            fclose(file); continue;
         }
-        ioStatus = fwrite(&waveId, 2, 1, file);
-        if(ioStatus == 0)
+        /* fmtLength == 24 */
+        static uint8_t fmtDescription[24];
+        ioStatus = fread(fmtDescription, 1, 24, file);
+        if(ioStatus < 24)
         {
-            printf("%s: Error writing to file.\n", argv[i]);
-            if(extraSize > 6) free(extraSection);
-            fclose(file);
-            continue;
+            printf("%s: Can't read fmt chunk.\n", argv[i]);
+            fclose(file); continue;
         }
+        uint16_t channels = *(uint16_t *)(fmtDescription+2),
+                 align = *(uint16_t *)(fmtDescription+12);
 
-        if(fseek(file, fmtOffset+18, SEEK_SET))
         {
-            printf("%s: Error using file?!\n", argv[i]);
-            if(extraSize > 6) free(extraSection);
-            fclose(file);
-            continue;
-        }
-        uint16_t sampleBlock = (align-4*channels)*8/(sampleBits*channels)+1;
-        ioStatus = fwrite(&sampleBlock, 2, 1, file);
-        if(ioStatus == 0)
-        {
-            printf("%s: Error writing to file.\n", argv[i]);
-            if(extraSize > 6) free(extraSection);
-            fclose(file);
-            continue;
-        }
+            uint16_t formatID = *(uint16_t *)fmtDescription,
+                     sampleBits = *(uint16_t *)(fmtDescription+14),
+                     extraSize = *(uint16_t *)(fmtDescription+16);
+            /* probably shouldn't accept 0x11 */
+            /* (formatID == 0x0002 || formatID == 0x0011) */
+            if(!( formatID == 0x0002 && sampleBits == 4 &&
+                        extraSize == 6 ))
+            {
+                printf("%s: File doesn't seem to be (Wwise) IMA ADPCM wave."
+                        " It has format ID 0x%04x, bits per sample %u, "
+                        "%u bytes of extra format info.\n",
+                        argv[i], formatID, sampleBits, extraSize);
+                fclose(file); continue;
+            }
+            /* wFormatTag */
+            *(uint16_t *)fmtDescription = waveId;
+            /* wSamplesPerBlock */
+            *(uint16_t *)(fmtDescription+18) = (align-4*channels)*8/(sampleBits*channels)+1;
 
-        if(channels > 1 && extraSize == 6)
-        {
-            uint32_t extraSectionPart1 = *(uint32_t *)extraSection;
-            uint16_t extraSectionPart2 = *(uint16_t *)(extraSection+4);
-            size_t comparison;
+            /* TODO: dwChannelMask */
+            /* NOTE: in Wwise files, when there is only one channel, data is
+             * structured the same way as described in IMA ADPCM specification,
+             * when there is more -- data is "clustered", but samples (not samples,
+             * but uint32 words worth of data) are not interleaved, they are
+             * clustered one channel after another */
 #if 0
-            comparison = 0;
-            static uint8_t compare[][6] = {
-                {0x00, 0x00, 0x03, 0x00, 0x00, 0x00},
-                {0x18, 0x00, 0x0b, 0x00, 0x00, 0x00},
-                {0x18, 0x00, 0x0c, 0x00, 0x00, 0x00},
-                {0x10, 0x00, 0x0b, 0x00, 0x00, 0x00},
-                {0x10, 0x00, 0x0c, 0x00, 0x00, 0x00}};
-            for(size_t count = 0; count < sizeof(compare)/sizeof(uint8_t[6]);
-                    ++count)
-            {
-                if(extraSectionPart1 == *(uint32_t *)compare[count] &&
-                        extraSectionPart2 == *(uint16_t *)(compare[count]+4))
-                {
-                    comparison = 1;
-                    break;
-                }
-            }
-#else
-            comparison = 1;
-            static uint8_t compare[][6] = {
-                /* probably only files with this as an extra section in fmt chunk
-                 * don't need to change data packing */
-                {0x00, 0x00, 0x04, 0x00, 0x00, 0x00},
-                {0x41, 0x00, 0x04, 0x00, 0x00, 0x00} // processed files
-            };
-            for(size_t count = 0; count < sizeof(compare)/sizeof(uint8_t[6]);
-                    ++count)
-            {
-                if(extraSectionPart1 == *(uint32_t *)compare[count] &&
-                        extraSectionPart2 == *(uint16_t *)(compare[count]+4))
-                {
-                    comparison = 0;
-                    break;
-                }
-            }
+            static uint8_t *extraSection = fmtDescription+18;
+            printf("%s ch:%u ", argv[i], channels);
+            for(size_t n = 0; n < extraSize; ++n)
+                printf("%02x", extraSection[n]);
+            printf("\n");
 #endif
-            if(extraSize > 6) free(extraSection);
-            if(comparison)
+
+            if(fseek(file, fmtOffset, SEEK_SET))
             {
-                /* shuffle data to make it interleaved */
-                if(fseek(file, 12, SEEK_SET))
+                printf("%s: Error using file?!\n", argv[i]);
+                fclose(file); continue;
+            }
+            ioStatus = fwrite(fmtDescription, 1, 24, file);
+            if(ioStatus == 0)
+            {
+                printf("%s: Error writing to file.\nAborting.\n", argv[i]);
+                fclose(file);
+                return 1;
+            }
+        }
+        if(ioStatus < 24)
+        {
+            printf("%s: Error writing to file.\nAborting.\n", argv[i]);
+            fclose(file);
+            return 1;
+        }
+
+        if(channels > 1)
+        { /* shuffle data to make it interleaved */
+                if(fseek(file, 4*3, SEEK_SET))
                 {
                     printf("%s: Error using file?!\n", argv[i]);
-                    fclose(file);
-                    continue;
+                    fclose(file); continue;
                 }
-                ioStatus = 1;
-                size_t dataLength = 0;
                 /* find "data" chunk */
-                while(1)
-                {
-                    uint32_t chunkHead[2];
-                    ioStatus = fread(chunkHead, 4, 2, file);
-                    if(ioStatus == 0)
-                    {
-                        printf("%s: Can't read enough of file.\n", argv[i]);
-                        break;
-                    }
-                    if(chunkHead[0] == *(uint32_t *)"data")
-                    {
-                        dataLength = chunkHead[1];
-                        break;
-                    }
-                    /* Not a data chunk */
-                    if(fseek(file, chunkHead[1], SEEK_CUR))
-                    {
-                        printf("%s: Can't read enough of file.\n", argv[i]);
-                        break;
-                    }
-                }
+                size_t dataLength = findChunk(dataMark, file, argv[i]);
                 if(dataLength == 0)
                 {
                     printf("%s: File has no usable data...\n", argv[i]);
-                    fclose(file);
-                    continue;
+                    fclose(file); continue;
                 }
-                if(align <= BUFFER_SIZE)
+                static uint8_t transformInData[BUFFER_SIZE], transformOutData[BUFFER_SIZE];
+                uint8_t *transformIn = transformInData,
+                        *transformOut = transformOutData;
+                /* how much blocks of data can be placed in buffers (integer divide) */
+                size_t transformCount = BUFFER_SIZE / align;
+                if(align > BUFFER_SIZE) /* just in case */
                 {
-                    static uint8_t transformIn[BUFFER_SIZE];
-                    static uint8_t transformOut[BUFFER_SIZE];
-                    /* how much blocks of data can be placed in buffers */
-                    size_t transformCount = BUFFER_SIZE/align;
-                    /* how much space these blocks will take up */
-                    size_t transformAmount = transformCount * align;
-                    for(size_t count = 0;
-                            count*transformAmount < dataLength;
-                            ++count)
+                    transformIn = malloc(2*align);
+                    transformOut = transformIn + align;
+                    transformCount = 1;
+                }
+                    for(size_t blockCount = 0;
+                            blockCount*align < dataLength; )
                     {
                         /* remember the place for the later write */
-                        size_t dataPoint = ftell(file);
+                        long dataPoint = ftell(file);
+                        if(dataPoint < 0)
+                        {
+                            printf("%s: Error using file?!\n", argv[i]);
+                            break;
+                        }
                         /* NOTE: Example of C runtime library weirdness
                          * (probably a bug, Linux x86-64, glibc 2.23): */
                         ioStatus = fread(transformIn, align, transformCount, file);
@@ -391,9 +314,12 @@ int main(int argc, char **argv)
                             break;
                         }
                         /* should tell you how much blocks of data
-                         * program have read */
+                         * the program have read */
                         size_t blockAmount = ioStatus;
-                        /* NOTE: assumes nBlockAlign is multiple of nChannels */
+                        /* NOTE: THIS IS WHERE WE INCREMENT THE LOOP INDEX */
+                        blockCount += blockAmount;
+                        /* NOTE: assumes nBlockAlign is multiple of nChannels
+                         * as required by the spec */
                         for(size_t block = 0; block < blockAmount; ++block)
                             for(size_t n = 0;
                                     /* NOTE: there is 4 in here because
@@ -413,45 +339,14 @@ int main(int argc, char **argv)
                         ioStatus = fwrite(transformOut, (blockAmount*align), 1, file);
                         if(ioStatus == 0)
                         {
-                            printf("%s: Error writing file.\n", argv[i]);
-                            break;
+                            printf("%s: Error writing file.\nAborting.\n", argv[i]);
+                            fclose(file); return 1;
                         }
                     }
-                } else { /* align > BUFFER_SIZE (just in case) */
-                    uint32_t *transformIn = malloc(2*align);
-                    uint32_t *transformOut = ((void *)transformIn) + align;
-                    for(size_t count = 0; count < dataLength/align; ++count)
-                    {
-                        /* remember the place for the later write */
-                        size_t dataPoint = ftell(file);
-                        ioStatus = fread(transformIn, align, 1, file);
-                        if(ioStatus == 0)
-                        {
-                            printf("%s: Unexpected end of file.\n", argv[i]);
-                            break;
-                        }
-                        /* NOTE: assumes nBlockAlign is multiple of nChannels */
-                        for(size_t n = 0; n < align/(channels*4); ++n)
-                            for(size_t s = 0; s < channels; ++s)
-                            {
-                                transformOut[n*channels+s] =
-                                    transformIn[s*align/(channels*4)+n];
-                            }
-                        if(fseek(file, dataPoint, SEEK_SET))
-                        {
-                            printf("%s: Error using file?!\n", argv[i]);
-                            break;
-                        }
-                        ioStatus = fwrite(transformOut, align, 1, file);
-                        if(ioStatus == 0)
-                        {
-                            printf("%s: Error writing file.\n", argv[i]);
-                            break;
-                        }
-                    }
+                if(align > BUFFER_SIZE) /* just in case */
+                {
                     free(transformIn);
                 }
-            }
         }
         fclose(file);
         printf("Finished processing %s\n", argv[i]);
